9d706414510596fda6d6d0081288596d0942aaa3
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-02 10:39:22 -0400

    Add project files.

---
+++ VisualStudio2019Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
