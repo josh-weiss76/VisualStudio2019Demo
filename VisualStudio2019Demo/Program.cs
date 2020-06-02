6a6f4e010ed92f986061d414d50e4786bf0c9058
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-02 10:46:50 -0400

    updated message

--- VisualStudio2019Demo/Program.cs
+++ VisualStudio2019Demo/Program.cs
@@ -7,6 +7,7 @@ namespace VisualStudio2019Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a change");
         }
     }
 }
